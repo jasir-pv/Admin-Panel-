40d91b935ef6a187b7f5efbf93f5b808667a9492
jasir-pv <jasirahsanpv40@gmail.com>
2024-10-03 08:44:37 +0530

    User information Components

--- src/components/widgetsLg/WidgetsLg.jsx
+++ src/components/widgetsLg/WidgetsLg.jsx
@@ -1,9 +1,60 @@
 import "./widgetsLg.css"
 
 export default function WidgetsLg() {
+
+  const Button = ({type}) =>{
+    return <button className={"widgetLgButton" + type}>{type}</button>
+  }
   return (
     <div className="widgetsLg">
-      lag
+      <h3 className="widgetLgTitle">Latest Transactions</h3>
+        <table className="widgetLgTable">
+          <tr className="widgetLgTr">
+            <th className="widgetLgTh">Customer</th>
+            <th className="widgetLgTh">Data</th>
+            <th className="widgetLgTh">Amount</th>
+            <th className="widgetLgTh">Stats</th>
+          </tr>
+          <tr className="widgetLgTr">
+            <td className="widgetUser">
+              <img src="https://avatars.githubusercontent.com/u/106402545?v=4" alt="" className="widgetLgImg" />
+              <span className="widgetLgName">Jasir</span>
+            </td>
+            <td className="widgetLgDate">2 jun 2024</td>
+            <td className="widgetLgAmount">$1022</td>
+            <td className="widgetLgStatus"><Button type="Approved" /></td>
+          </tr>
+         
+          <tr className="widgetLgTr">
+            <td className="widgetUser">
+              <img src="https://avatars.githubusercontent.com/u/106402545?v=4" alt="" className="widgetLgImg" />
+              <span className="widgetLgName">Jasir</span>
+            </td>
+            <td className="widgetLgDate">2 jun 2024</td>
+            <td className="widgetLgAmount">$1022</td>
+            <td className="widgetLgStatus"><Button type="Declined" /></td>
+          </tr>
+         
+          <tr className="widgetLgTr">
+            <td className="widgetUser">
+              <img src="https://avatars.githubusercontent.com/u/106402545?v=4" alt="" className="widgetLgImg" />
+              <span className="widgetLgName">Jasir</span>
+            </td>
+            <td className="widgetLgDate">2 jun 2024</td>
+            <td className="widgetLgAmount">$1022</td>
+            <td className="widgetLgStatus"><Button type="Pending" /></td>
+          </tr>
+         
+          <tr className="widgetLgTr">
+            <td className="widgetUser">
+              <img src="https://avatars.githubusercontent.com/u/106402545?v=4" alt="" className="widgetLgImg" />
+              <span className="widgetLgName">Jasir</span>
+            </td>
+            <td className="widgetLgDate">2 jun 2024</td>
+            <td className="widgetLgAmount">$1022</td>
+            <td className="widgetLgStatus"><Button type="Approved" /></td>
+          </tr>
+        </table>
     </div>
   )
 }
--- src/components/widgetsSm/WidgetsSm.jsx
+++ src/components/widgetsSm/WidgetsSm.jsx
@@ -7,14 +7,56 @@ export default function WidgetsSm() {
       <span className="widgetTitle">New Join Members</span>
       <ul className="widgetSmList">
         <li className="widgetSmListItems">
-                <img src="" alt="" />
+                <img src="https://avatars.githubusercontent.com/u/106402545?v=4" alt=""  className="widgetSmImg"/>
                 <div className="widgetSmUser">
                     <span className="widgetSmUsername"> Anna keller</span>
                     <span className="widgetSmUserTitle">Softwear Engineer</span>
 
                 </div>
                 <button className="widgetSmButton">
-                    <Visibility/>
+                    <Visibility className="widgetIcon"/>
+                    Display
+                </button>
+        </li>
+      </ul>
+      <ul className="widgetSmList">
+        <li className="widgetSmListItems">
+                <img src="https://avatars.githubusercontent.com/u/106402545?v=4" alt=""  className="widgetSmImg"/>
+                <div className="widgetSmUser">
+                    <span className="widgetSmUsername"> Anna keller</span>
+                    <span className="widgetSmUserTitle">Softwear Engineer</span>
+
+                </div>
+                <button className="widgetSmButton">
+                    <Visibility className="widgetIcon"/>
+                    Display
+                </button>
+        </li>
+      </ul>
+      <ul className="widgetSmList">
+        <li className="widgetSmListItems">
+                <img src="https://avatars.githubusercontent.com/u/106402545?v=4" alt="img"  className="widgetSmImg"/>
+                <div className="widgetSmUser">
+                    <span className="widgetSmUsername"> Anna keller</span>
+                    <span className="widgetSmUserTitle">Softwear Engineer</span>
+
+                </div>
+                <button className="widgetSmButton">
+                    <Visibility className="widgetIcon"/>
+                    Display
+                </button>
+        </li>
+      </ul>
+        <ul className="widgetSmList">
+        <li className="widgetSmListItems">
+                <img src="https://avatars.githubusercontent.com/u/106402545?v=4" alt=""  className="widgetSmImg"/>
+                <div className="widgetSmUser">
+                    <span className="widgetSmUsername"> Anna keller</span>
+                    <span className="widgetSmUserTitle">Softwear Engineer</span>
+
+                </div>
+                <button className="widgetSmButton">
+                    <Visibility className="widgetIcon"/>
                     Display
                 </button>
         </li>
